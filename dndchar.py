a3b0b4cc8eccf6bb9f22be738c1d210e445ce7b4
Buddy <apruitt0321@gmail.com>
2016-07-01 14:03:43 -0600

    Testing installation of lxml and requests for web scraping.

--- dndchar.py
+++ dndchar.py
@@ -1,2 +1,9 @@
 # An automated character sheet for Pathfinder. Goals include automatic fill-in for stats, roll generation,
-# and automatic class-specific skill attribution. 
+# and automatic class-specific skill attribution.
+
+
+from lxml import html
+import requests
+
+print "Hello World!"
+
