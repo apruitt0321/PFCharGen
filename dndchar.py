14feafed0f0b7e7fd38bd0c7a46c2d9dc0ddcdc8
Buddy <apruitt0321@gmail.com>
2016-07-01 15:40:56 -0600

    Playing around with sqlite3 databases.

--- dndchar.py
+++ dndchar.py
@@ -2,8 +2,31 @@
 # and automatic class-specific skill attribution.
 
 
-from lxml import html
-import requests
+import sqlite3
+conn = sqlite3.connect('example.db')
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
+#c.execute("INSERT INTO spells VALUES ('Magic Missle', 'Kills enemy dead', '30ft', 'Instant')")
+#conn.commit()
 
-print "Hello World!"
+def check_table():
+    try:
+        c.execute("CREATE TABLE spells (spellname, spelleffect, range, duration)")
+        return False
+    except:
+        return True
+
+if check_table() == True:
+    print "Hello World!"
+else:
+    print "Something went wrong!"
+
+t = ('30ft',)
+c.execute('SELECT * FROM spells WHERE range=?', t)
+r = c.fetchone()
+
+print r[0]
+
+#for i in r:
+ #   print i
 
